08456287c2f5c2ff43acca8acc479155b7618f47
Tom Hunt <tom@visualr.co.uk>
2025-02-27 08:34:33 +0000

    Select elements from the DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select DOM Elements
+let btn = document.querySelector(".btn");
+let ac = document.querySelector(".ac");
+let percentage = document.querySelector(".percentage");
+let plusMinus = document.querySelector(".plus-minus");
+let operator = document.querySelector(".operator");
+let equals = document.querySelector(".equals");
+
