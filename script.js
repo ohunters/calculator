09b7ca037bbf480b8b68870472e01f8dce9b686b
Tom Hunt <tom@visualr.co.uk>
2025-02-27 09:38:10 +0000

    Create logic for updating the display when the button is pressed

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Select DOM Elements
-let btn = document.querySelector(".btn");
+let display = document.querySelector(".display")
+let btns = document.querySelectorAll(".btn");
 let ac = document.querySelector(".ac");
 let percentage = document.querySelector(".percentage");
 let plusMinus = document.querySelector(".plus-minus");
 let operator = document.querySelector(".operator");
 let equals = document.querySelector(".equals");
 
+
+// Add Event Listeners
+btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        let btnPressed = event.target;
+        let btnValue = btnPressed.textContent;
+
+        display.textContent = "";
+
+        if (btn.classList.contains("ac") || display === 0) {
+            display.textContent = ""
+        } else {
+            display.textContent += btnValue;
+        }
+        
+    })
+});
